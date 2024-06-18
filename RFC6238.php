7097a416ebdd7f17a8494be690f70054b021ab6d
Jonathan <jseder@gmail.com>
2024-06-18 14:05:35 -0700

    Update with David Shim's QR code library

--- RFC6238.php
+++ RFC6238.php
@@ -1,22 +1,20 @@
 <?php
 
-define('TIMESTEP',           '30');     // seconds
-define('TIMEWINDOW',          '2');     // 2 steps either way
-define('TOKENLENGTH',         '6');     // characters
+define('TIMESTEP',    '30');     // seconds
+define('TIMEWINDOW',   '2');     // 2 steps either way
+define('TOKENLENGTH',  '6');     // characters
 define('TOKENCEILING',  '1000000');     // 10**TOKENLENGTH
 
-// Secret key...  Must be retained on the server in plain text.
-
-
-$UserSecretKey = "FWBXWGAQDSNLQQCQFXZW234";  // Must be [A-Z][2-7] !
-
+$myip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+$shash = hash('sha256', 'fwb~$_,xw'.$myip);
+$UserSecretKey = strtolower(substr($shash,0,32));
+$UserSecretKey = str_replace(['0','1','8','9'],['g','q','o','l'],$UserSecretKey);
 
 function TimeStamp() { return floor(microtime(true)/TIMESTEP); }
 
-
 class RFC6238 {
 
-   public static function B32toB($inarg) {
+   public static function b32_decode($inarg) {
       $inarg  = strtoupper($inarg);
       if (!preg_match('/^[A-Z2-7]+$/', $inarg))
          throw new Exception('Invalid characters in the base32 string.');
@@ -36,8 +34,7 @@ class RFC6238 {
       return $result;
       }
 
-// HMAC-Based One-Time Password Algorithm from RFC 4226
-   public static function MakeOTP($secretKey, $timeval) {
+   public static function oath_hotp($secretKey, $timeval) {
       if (strlen($secretKey) < 8)
          throw new Exception('Secret key must contain at least 16 base 32 characters');
       $bTimeval = pack('N*', 0) . pack('N*', $timeval);    // Timeval must be 64-bit int
@@ -53,11 +50,11 @@ class RFC6238 {
       }
 
 
-   public static function TestKey($secretKey, $testkey) {
+   public static function verify_key($secretKey, $testkey) {
       $tStamp = TimeStamp();
-      $bSecretKey = self::B32toB($secretKey);
+      $bSecretKey = self::b32_decode($secretKey);
       for ($ts=-TIMEWINDOW; $ts<=TIMEWINDOW; $ts++)
-         if (self::MakeOTP($bSecretKey, $tStamp+$ts) == $testkey) return true;
+         if (self::oath_hotp($bSecretKey, $tStamp+$ts) == $testkey) return true;
       return false;
       }
 
@@ -67,46 +64,56 @@ class RFC6238 {
 // Main program starts here
 
 if(isset($_REQUEST['totp'])) {
-   $secretkey = RFC6238::B32toB($UserSecretKey);   // Decode it into binary
-   $otp       = RFC6238::MakeOTP($secretkey, TimeStamp());  // Get current token
+   $secretkey = RFC6238::b32_decode($UserSecretKey);   // Decode it into binary
+   $otp       = RFC6238::oath_hotp($secretkey, TimeStamp());  // Get current token
    $testkey   = $_REQUEST['totp'];
-   echo(" Secret Init key: $UserSecretKey");
-   echo("<br>One time password: $otp");
-   echo("<br>Challenge: $testkey<br>");
-   $result = RFC6238::TestKey($UserSecretKey, $testkey);
-   echo $result ? '<strong>Success!</strong>' : '<span style="color: red">Failure.</span>';
+   echo("<table><tr><td>Correct value:<td>$otp");
+   echo("<tr><td>You entered:<td>$testkey");
+   $result = RFC6238::verify_key($UserSecretKey, $testkey);
+   echo '<tr><th>';
+   echo $result ? 'Success!' : 'Failure';
+   echo '</table>';
    return;
    }
 ?>
 
 <html><head><title>TOTP PHP demo</title>
-<style>
-   * { font-family: sans-serif; }
-   .k { font-family: monospace;  }
-   </style>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs/qrcode.min.js"></script>
+
 <script>
-   function CheckAuth() {
-      var telem = document.getElementById('authin').value;
-      $.get('RFC6238.php',
-         { totp: telem },
-         function(data) { msg.innerHTML = data; },
-         "html");
-      }
+function CheckAuth() {
+   var telem = document.getElementById('authin').value;
+   $.get('totp_auth.php',
+      { totp: telem },
+      function(data) { msg.innerHTML = data; },
+      "html");
+   }
+
+function MakeQR(divid,secret) {
+   const qrcode = new QRCode(divid, {
+     text: 'otpauth://totp/yournamehere?secret='+secret,
+     width: 128,
+     height: 128,
+     colorDark : '#000',
+     colorLight : '#fff',
+     correctLevel : QRCode.CorrectLevel.H
+     });
+   }
+$(document).ready(function() {
+   MakeQR(document.getElementById('qrcode'), $('#secret').text() )
+   });
    </script>
 </head>
 <body>
 <p>
-<p> &nbsp;
-<p> &nbsp;
+<h3>Scan this QR code to set up TOTP authentication:</h3>
+<div id="qrcode" style="display: block; margin-left: 50px;"></div>
 <?php
-echo '<img src="https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/yournamehere?secret='.$UserSecretKey.'" alt="fail">';
-echo '<p> &nbsp; &nbsp; <span class="k">'.$UserSecretKey.'</span>';
+echo '<p><div> If you can\'t scan the barcode: &nbsp; <div style="font-family: monospace; display: inline;" id="secret">'.$UserSecretKey.'</div></div>';
 ?>
-<p> <p>Use the QR code or secret key above to set up your authenticator.</a>
-<p><label for="authin">Then check an authentication code: </label><input type="text" length=6 id="authin">
+<p><label for="authin">Check an authentication code: </label><input type="text" length=6 id="authin">
  &nbsp; <input type='button' value="Check" onclick='CheckAuth()'>
- <p> &nbsp; <p>
-<div id="msg" style="border: 1px solid blue; height: 120px; width: 400px; padding: 10px"></div>
+<p><div id="msg" style="border: 1px solid blue; height: 70px; width: 250px; padding: 10px"></div>
 </body>
 </html>
